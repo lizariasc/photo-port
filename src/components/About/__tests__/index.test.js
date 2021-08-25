40569515cb9f035da89dfde82d07f30bd918e038
Liz A <lizethariasc14@gmail.com>
2021-08-24 21:52:22 -0700

    testing environment

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+// component we will be testing
+import About from '..';
+
+// ensure that after each test, we won't have any leftover memory data that could give us false results
+afterEach(cleanup);
+
+// declare the component we're testing
+describe('About component', () => {
+    //renders About test
+  })
+  
