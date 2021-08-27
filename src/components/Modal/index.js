1f4fb6edab8f9eb67a6288c5f2368e466385587e
Liz A <lizethariasc14@gmail.com>
2021-08-27 13:39:28 -0700

    tests

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Modal = ({ onClose, currentPhoto }) => {
-    const {name, category, description, index} = currentPhoto;
+const {name, category, description, index} = currentPhoto;
 
     return (
+        
         <div className="modalBackdrop">
         <div className="modalContainer">
         <h3 className="modalTitle">{name}</h3>
