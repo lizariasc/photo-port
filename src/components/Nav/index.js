fc28b097edf894db08dd2b405cf254ba341afe27
Liz A <lizethariasc14@gmail.com>
2021-08-27 12:09:38 -0700

    conditional styling

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,13 +6,10 @@ function Nav(props) {
     categories = [],
     setCurrentCategory,
     currentCategory,
+    contactSelected,
+    setContactSelected
   } = props;
 
-
-  const handleClick = (item) => {
-    console.log(item);
-    return item;
-  };
 
   return (
     <header className="flex-row px-1">
@@ -23,26 +20,27 @@ function Nav(props) {
       </h2>
       <nav>
         <ul className="flex-row">
-          <li className="mx-2">
-            <a data-testid="about" href="#about">
+        <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)} >
               About me
             </a>
           </li>
           <li className={"mx-2"}>
-            <span onClick={() => handleClick('Contact')}>
+            <span onClick={() => setContactSelected(true)}>
               Contact
             </span>
           </li>
           {categories.map((category) => (
-            <li
-              className={`mx-1 ${
-                currentCategory.name === category.name
-                }`}
-              key={category.name}
-            >
+          <li
+          className={`mx-1 ${
+            currentCategory.name === category.name && !contactSelected && `navActive`
+            }`}
+          key={category.name}
+        >
               <span
                 onClick={() => {
                   setCurrentCategory(category);
+                  setContactSelected(false);
                 }}
               >
                 {capitalizeFirstLetter(category.name)}
