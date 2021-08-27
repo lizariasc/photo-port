4a376f4148b5248026b57082dd0be9bc1d94b1fd
Liz A <lizethariasc14@gmail.com>
2021-08-27 10:07:16 -0700

    display photos

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,25 +1,19 @@
+// __tests__/About.test.js
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-
-// component we will be testing
 import About from '..';
 
-// ensure that after each test, we won't have any leftover memory data that could give us false results
 afterEach(cleanup);
 
-// declare the component we're testing
-describe('About component', () => {
-    // First Test: verify that the component is rendering
-    it('renders', () => {
-        render(<About />);
-      });
-
-  // Second Test: "test case", we'll compare snapshot versions of the DOM node structure
+describe('About component renders', () => {
+  it('renders', () => {
+    render(<About />);
+  });
+  
   it('matches snapshot DOM node structure', () => {
-    // render About
     const { asFragment } = render(<About />);
+    
     expect(asFragment()).toMatchSnapshot();
   });
-});
-  
+})
